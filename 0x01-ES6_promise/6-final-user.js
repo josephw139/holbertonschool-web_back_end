283f2692c7c8d907fff846536f8b96c02e42efe8
3599josephw <3599@holbertonschool.com>
2022-05-06 11:08:59 -0500

    replace reason with error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const myObjs = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+    .then((values) => {
+      return values
+    });
 
+  let error = String(myObjs[1].reason);
+  delete myObjs[1].reason;
+  myObjs[1]['value'] = error;
+  console.log(myObjs);
   return myObjs;
 }
