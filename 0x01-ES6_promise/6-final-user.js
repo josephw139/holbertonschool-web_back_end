f9a1b9f84c249604b808e82818c40647eb8921f9
3599josephw <3599@holbertonschool.com>
2022-05-06 10:28:46 -0500

    using Promise.allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => values);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values);
 }
