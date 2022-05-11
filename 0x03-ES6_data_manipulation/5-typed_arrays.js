948365d47ed57c760866da16e489791339d10056
3599josephw <3599@holbertonschool.com>
2022-05-11 11:58:21 -0500

    error catch

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position > length) {
+    throw new Error('Position outside range');
+  }
   const buff = new ArrayBuffer(length);
   const int8 = new Int8Array(buff);
   int8[position] = value;
