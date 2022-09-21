0b270a6ff0fc339f9b6240bd64f23ed56150f7a9
3599josephw <3599@holbertonschool.com>
2022-09-21 10:14:05 -0500

    pep8

--- 0x04-Session_authentication/api/v1/app.py
+++ 0x04-Session_authentication/api/v1/app.py
@@ -30,10 +30,11 @@ def before_request():
     """ before request """
     if auth and auth.require_auth(
             request.path, ['/api/v1/status/', '/api/v1/unauthorized/',
-                           '/api/v1/forbidden/', '/api/v1/auth_session/login/']):
+                           '/api/v1/forbidden/',
+                           '/api/v1/auth_session/login/']):
 
         if auth.authorization_header(request) is None and auth.session_cookie(
-            request) is None:
+                request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
