dda0798d94d5e76925a93e3a233d266675142168
3599josephw <3599@holbertonschool.com>
2022-09-26 13:53:03 -0500

    if/else to try/except

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -65,12 +65,13 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """ generates a reset password token """
-        user = self._db.find_user_by(email=email)
-        if user:
+        try:
+            user = self._db.find_user_by(email=email)
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
             return token
-        raise ValueError
+        except Exception:
+            raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
         """ updates the password """
