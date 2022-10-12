46695510e1dfa7a775248e2e87fd0b0190b44334
3599josephw <3599@holbertonschool.com>
2022-10-12 15:24:37 -0500

    IF NOT EXISTS

--- 0x01-MySQL_Advanced/6-bonus.sql
+++ 0x01-MySQL_Advanced/6-bonus.sql
@@ -4,15 +4,13 @@ DELIMITER //
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 	BEGIN
-		SET @project_id := (SELECT id FROM projects WHERE name = project_name);
-		IF @project_id IS NULL THEN
+		DECLARE project_id INT;
+		IF NOT EXISTS (SELECT * FROM projects WHERE name = project_name) THEN
 			INSERT INTO projects (name) VALUES (project_name);
-			SET @project_id := LAST_INSERT_ID();
 		END IF;
 
-
-		INSERT INTO corrections (user_id, project_id, score)
-		VALUES (user_id, @project_id, score)
+		SELECT id INTO project_id FROM projects WHERE name = project_name;
+		INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 	END //
 
 DELIMITER ;
