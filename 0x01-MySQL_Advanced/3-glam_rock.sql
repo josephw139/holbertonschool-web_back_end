3fac0a3d59fff2f1153241d18b2c2db073160ec1
3599josephw <3599@holbertonschool.com>
2022-10-12 15:03:56 -0500

    style LIKE

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- lists all bands 'Glam rock' as main style
 
 SELECT band_name, (IFNULL(split, 2020) - formed) as lifespan FROM
-metal_bands WHERE style = 'Glam rock' ORDER BY lifepsan DESC;
+metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifepsan DESC;
