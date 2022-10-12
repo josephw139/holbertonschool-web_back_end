5cb89f79502216598ae64698c9bb8eea9515cb26
3599josephw <3599@holbertonschool.com>
2022-10-12 15:09:24 -0500

    quantity typo

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -2,5 +2,5 @@
 
 CREATE TRIGGER trigger AFTER INSERT ON orders
 FOR EACH ROW UPDATE items
-SET quanity = quanity - NEW.number WHERE
+SET quantity = quantity - NEW.number WHERE
 name = NEW.item_name
