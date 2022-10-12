a7911844b950000fb505e8d9e3af2e236ddfa35c
3599josephw <3599@holbertonschool.com>
2022-10-12 15:28:30 -0500

    fix

--- 0x01-MySQL_Advanced/7-average_score.sql
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -4,8 +4,9 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 	BEGIN
-		SET @avg_score := (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
-		UPDATE users SET average_score = @avg_score WHERE id = user_id
+		DECLARE average FLOAT;
+		SET average = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
+		UPDATE users SET average_score = average WHERE id = user_id;
 	END //
 
 DELIMITER ;
