9929064097ad74bcf31675acde5cc8916fa25f9c
3599josephw <3599@holbertonschool.com>
2022-10-24 18:34:31 -0500

    eslint

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -3,10 +3,10 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-    let line = process.stdin.read();
-    if (line) {
-      process.stdout.write(`Your name is: ${line}`);
-    }
-  }).on('end', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
+  const line = process.stdin.read();
+  if (line) {
+    process.stdout.write(`Your name is: ${line}`);
+  }
+}).on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
