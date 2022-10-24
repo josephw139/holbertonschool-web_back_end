3dc38c8f8e40c59cb1174f8337a55d5d51b959d5
3599josephw <3599@holbertonschool.com>
2022-10-24 18:37:46 -0500

    eslint

--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -1,33 +1,33 @@
 // Reading a file synchronously with Node JS
 
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
-  let data = [];
+  const data = [];
 
   if (!(fs.existsSync(path))) {
     throw new Error('Cannot load the database');
   }
   const db = fs.readFileSync(path, 'utf8');
 
-  let rows = db.split('\n');
+  const rows = db.split('\n');
 
-  rows.forEach(row => {
+  rows.forEach((row) => {
     if (row.length > 0) {
       data.push(row.split(','));
     }
-  })
+  });
 
-  let cs = [];
-  let swe = [];
-  data.forEach(row => {
+  const cs = [];
+  const swe = [];
+  data.forEach((row) => {
     if (row.includes('CS')) {
       cs.push(row[0]);
     }
     if (row.includes('SWE')) {
       swe.push(row[0]);
     }
-  })
+  });
   console.log(`Number of students: ${data.length - 1}`);
   console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
   console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
