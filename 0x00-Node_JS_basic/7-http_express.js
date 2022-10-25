d25a50e517259e572874ced1e2b05cf8c522aad4
3599josephw <3599@holbertonschool.com>
2022-10-24 19:20:32 -0500

    using error.message

--- 0x00-Node_JS_basic/7-http_express.js
+++ 0x00-Node_JS_basic/7-http_express.js
@@ -17,9 +17,8 @@ app.get('/students', async (req, res) => {
     res.write(`Number of students in CS: ${values[1].length}. List: ${values[1].join(', ')}\n`);
     res.write(`Number of students in SWE: ${values[2].length}. List: ${values[2].join(', ')}`);
     res.end();
-  }).catch(() => {
-    res.write('This is the list of our students\nCannot load the database');
-    res.end();
+  }).catch((error) => {
+    res.end('This is the list of our students\n' + error.message);
   });
 });
 
