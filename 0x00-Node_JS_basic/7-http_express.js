5dc6aeee5110ad713c1b471ab90b8e657b612912
3599josephw <3599@holbertonschool.com>
2022-10-24 19:17:54 -0500

    moved res.end()

--- 0x00-Node_JS_basic/7-http_express.js
+++ 0x00-Node_JS_basic/7-http_express.js
@@ -10,17 +10,17 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
+app.get('/students', async (req, res) => {
   res.write('This is the list of our students\n');
-  countStudents(process.argv[2]).then((values) => {
+  await countStudents(process.argv[2]).then((values) => {
     res.write(`Number of students: ${values[0].length - 1}\n`);
     res.write(`Number of students in CS: ${values[1].length}. List: ${values[1].join(', ')}\n`);
     res.write(`Number of students in SWE: ${values[2].length}. List: ${values[2].join(', ')}`);
+    res.end();
   }).catch(() => {
     res.write('This is the list of our students\nCannot load the database');
     res.end();
   });
-  res.end();
 });
 
 app.listen(1245);
