8e2ea573990bc087800f726cff9fbcc765e52d8f
3599josephw <3599@holbertonschool.com>
2022-10-24 19:16:47 -0500

    removed async

--- 0x00-Node_JS_basic/7-http_express.js
+++ 0x00-Node_JS_basic/7-http_express.js
@@ -10,9 +10,9 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
+app.get('/students', (req, res) => {
   res.write('This is the list of our students\n');
-  await countStudents(process.argv[2]).then((values) => {
+  countStudents(process.argv[2]).then((values) => {
     res.write(`Number of students: ${values[0].length - 1}\n`);
     res.write(`Number of students in CS: ${values[1].length}. List: ${values[1].join(', ')}\n`);
     res.write(`Number of students in SWE: ${values[2].length}. List: ${values[2].join(', ')}`);
