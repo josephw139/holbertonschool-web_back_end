0b7749aebdd4a7af4d40228d68992975f2a7f7ce
3599josephw <3599@holbertonschool.com>
2022-10-24 19:02:12 -0500

    eslint

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -5,18 +5,18 @@ const process = require('process');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  if (req.url == '/') {
+  if (req.url === '/') {
     res.write('Hello Holberton School!');
     res.end();
   }
-  if (req.url == '/students') {
+  if (req.url === '/students') {
     res.write('This is the list of our students\n');
     await countStudents(process.argv[2]).then((values) => {
       res.write(`Number of students: ${values[0].length - 1}\n`);
       res.write(`Number of students in CS: ${values[1].length}. List: ${values[1].join(', ')}\n`);
       res.write(`Number of students in SWE: ${values[2].length}. List: ${values[2].join(', ')}`);
     }).catch(() => {
-      res.write(`This is the list of our students\nCannot load the database`);
+      res.write('This is the list of our students\nCannot load the database');
       res.end();
     });
     res.end();
