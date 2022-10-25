193f08ca7004e34e31dbfa4876226fe58039a40f
3599josephw <3599@holbertonschool.com>
2022-10-24 19:29:36 -0500

    eslint

--- 0x00-Node_JS_basic/full_server/server.js
+++ 0x00-Node_JS_basic/full_server/server.js
@@ -1,8 +1,9 @@
 // express server
 
 const express = require('express');
-const app = express();
 const routes = require('./routes/index');
+
+const app = express();
 
 app.use(routes);
 app.listen(1245);
