c9d3d6abae0baf364702378af0ec2bb4c35469ac
3599josephw <3599@holbertonschool.com>
2022-10-24 19:26:07 -0500

    eslint

--- 0x00-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x00-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,22 +1,21 @@
 // Student Controller
 import readDatabase from '../utils';
 
-
 class StudentsController {
   static getAllStudents(request, response) {
     response.status(200);
-    readDatabase(process.argv[2]).then(values => {
+    readDatabase(process.argv[2]).then((values) => {
       response.write(`Number of students: ${values[0].length - 1}\n`);
       response.write(`Number of students in CS: ${values[1].length}. List: ${values[1].join(', ')}\n`);
       response.write(`Number of students in SWE: ${values[2].length}. List: ${values[2].join(', ')}`);
       response.end();
-    })
+    });
   }
 
   static getAllStudentsByMajor(request, response) {
     response.status(200);
     const major = request.params;
-    if (major.major != 'CS' && major.major != 'SWE') {
+    if (major.major !== 'CS' && major.major !== 'SWE') {
       response.status(500);
       response.write('Major parameter must be CS or SWE');
       response.end();
@@ -24,14 +23,14 @@ class StudentsController {
     }
     readDatabase(process.argv[2]).then((values) => {
       let num;
-      if (major.major == 'CS') {
+      if (major.major === 'CS') {
         num = 1;
       } else {
         num = 2;
       }
       response.write(`List: ${values[num].join(', ')}`);
       response.end();
-    })
+    });
   }
 }
 
