115fd86ce8f87fab53857b1c56560b192a33e04b
3599josephw <3599@holbertonschool.com>
2022-10-24 19:10:23 -0500

    eslint

--- 0x00-Node_JS_basic/7-http_express.js
+++ 0x00-Node_JS_basic/7-http_express.js
@@ -1,9 +1,10 @@
 // express server
 
 const express = require('express');
-const app = express();
 const process = require('process');
 const countStudents = require('./3-read_file_async');
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -11,15 +12,15 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
   res.write('This is the list of our students\n');
-    await countStudents(process.argv[2]).then(values => {
-      res.write(`Number of students: ${values[0].length - 1}\n`);
-      res.write(`Number of students in CS: ${values[1].length}. List: ${values[1].join(', ')}\n`);
-      res.write(`Number of students in SWE: ${values[2].length}. List: ${values[2].join(', ')}`);
-    }).catch(() => {
-      res.write('This is the list of our students\nCannot load the database');
-      res.end();
-    });
+  await countStudents(process.argv[2]).then(values => {
+    res.write(`Number of students: ${values[0].length - 1}\n`);
+    res.write(`Number of students in CS: ${values[1].length}. List: ${values[1].join(', ')}\n`);
+    res.write(`Number of students in SWE: ${values[2].length}. List: ${values[2].join(', ')}`);
+  }).catch(() => {
+    res.write('This is the list of our students\nCannot load the database');
     res.end();
+  });
+  res.end();
 })
 
 app.listen(1245);
