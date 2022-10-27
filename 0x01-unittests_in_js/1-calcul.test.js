36c337a07f6e28fb5e2fc1da26d7a5a550673cbc
3599josephw <3599@holbertonschool.com>
2022-10-27 12:40:15 -0500

    task 1

---
+++ 0x01-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const calculateNumber = require('./1-calcul.js');
+
+
+describe("calculateNumber", function() {
+  it("checks add", function() {
+    assert.equal(calculateNumber('SUM', 13, 56), 69);
+    assert.equal(calculateNumber('SUM', 12.6, 56.3), 69);
+  });
+
+  it("checks subtract", function() {
+    assert.equal(calculateNumber('SUBTRACT', -13, -56), 43);
+    assert.equal(calculateNumber('SUBTRACT', -12.6, -56.3), 43);
+  });
+
+  it("checks divide", function() {
+    assert.equal(calculateNumber('DIVIDE', -10, 2), -5);
+    assert.equal(calculateNumber('DIVIDE', -9.7, 2.1), -5);
+  });
+});
