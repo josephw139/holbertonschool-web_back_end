6c80411e175a2c037a202c953cc147456c813eb6
3599josephw <3599@holbertonschool.com>
2022-10-28 10:01:20 -0500

    task 9

--- 0x01-unittests_in_js/9-api/api.js
+++ 0x01-unittests_in_js/9-api/api.js
@@ -10,4 +10,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id([0-9]*)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
 module.exports = app;
---
+++ 0x01-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('app', () => {
+  it('app test - GET /', (done) => {
+    request('http://localhost:7865/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('app test - GET /cart/id', (done) => {
+    request('http://localhost:7865/cart/1', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+    })
+  });
+
+  it('app test - GET /cart/id - bad id', (done) => {
+    request('http://localhost:7865/cart/no', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+    })
+  })
+});
